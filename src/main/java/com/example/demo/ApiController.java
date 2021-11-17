e4af26b8c9ec010810a52f089ce1a8908f37697d
migavrilov <migav@icloud.com>
2021-11-17 12:01:44 +0300

    back

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -1,72 +1,106 @@
 package com.example.demo;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 @RestController
-@RequestMapping("/api")
 public class ApiController {
-    private final TutorialRepository tutorialRepository;
-    public ApiController(TutorialRepository tutorialRepository)
-    {
-        this.tutorialRepository = tutorialRepository;
+
+    ArrayList<User> users = new ArrayList<>();
+
+    @PutMapping("users")
+    public void addUser(@RequestBody NewUser newUser) {
+        String rp = newUser.getRepassword();
+        if (!(rp.equals(newUser.getPassword()))) {
+            throw new NotSamePasswordException();
+        }
+        if (!(newUser.getUsername().matches("[A-Za-z]+"))) {
+            throw new BadLettersException();
+        }
+        if (this.users.size() != 0) {
+            for (int i = 0; i < this.users.size(); i++) {
+                if (this.users.get(i).getUsername().equals(newUser.getUsername())) {
+                    throw new ThisUserAlreadyExistsException();
+                }
+            }
+        }
+
+        User userToAdd = new User(newUser.getUsername(), newUser.getPassword(), newUser.getAge());
+        users.add(userToAdd);
     }
 
-    @GetMapping("/tutorials")
-    public List<Tutorial> getAllTutorials(@RequestParam(required =
-            false) String title) {
-        List<Tutorial> tutorials = new ArrayList<Tutorial>();
-        if (title == null) {
-            tutorials.addAll(tutorialRepository.findAll());
-        } else {
-            tutorials.addAll(tutorialRepository.findByTitleContaining(title));
+    @PostMapping("users/{username}")
+    public String getUser(@PathVariable("username") String username) {
+        for (int i = 0; i < this.users.size(); i++) {
+            if (this.users.get(i).getUsername().equals(username)) {
+                return this.users.get(i).toString();
+            }
         }
-        return tutorials;
+
+        throw new UserNotFoundException();
     }
-    @GetMapping("/tutorials/{id}")
-    public Tutorial getTutorialById(@PathVariable("id") long id) {
-        Optional<Tutorial> tutorialData =
-                tutorialRepository.findById(id);
-        if (tutorialData.isPresent()) {
-            return tutorialData.get();
+
+    @DeleteMapping("users/{usernameToTakeAction}/{username}")
+    public void deleteUser(@PathVariable("username") String username, @PathVariable("usernameToTakeAction") String usernameToTakeAction) {
+        if (!(usernameToTakeAction.startsWith("admin"))) {
+            throw new NotAllowedException();
+        }
+        for (int i = 0; i < this.users.size(); i++) {
+            if (this.users.get(i).getUsername().equals(username)) {
+                this.users.remove(i);
+                return;
+            }
+        }
+        throw new UserNotFoundException();
+    }
+
+    @PutMapping("users/{usernameToTakeAction}/{usernameToUpdate}/{newPassword}")
+    public void updatePassword(@PathVariable("usernameToTakeAction") String usernameToTakeAction, @PathVariable("usernameToUpdate") String usernameToUpdate, @PathVariable("newPassword") String newPassword) {
+        if (!(usernameToTakeAction.startsWith("update"))) {
+            throw new NotAllowedException();
+        }
+        if (!(usernameToUpdate.matches("[A-Za-z]+"))) {
+            throw new BadLettersException();
+        }
+        for (int i = 0; i < this.users.size(); i++) {
+            if (this.users.get(i).getUsername().equals(usernameToUpdate)) {
+                this.users.get(i).setPassword(newPassword);
+                return;
+            }
+        }
+        throw new UserNotFoundException();
+
+    }
+
+    @GetMapping("users/{age}")
+    public String getUsers(@PathVariable("age") int age, @RequestParam(required = false) String direction) {
+        String toReturn = "";
+        if (direction != null && direction.equals("up")) {
+            for (int i = this.users.size() - 1; i >= 0; i--) {
+                if  (this.users.get(i).getAge() >= age - 5 && this.users.get(i).getAge() <= age +5) {
+                    toReturn = toReturn.concat(this.users.get(i).toString());
+                    toReturn = toReturn.concat("\n");
+                }
+            }
         } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND); }
+            for (int i = 0; i < this.users.size(); i++) {
+                if (this.users.get(i).getAge() >= age - 5 && this.users.get(i).getAge() <= age + 5) {
+                    toReturn = toReturn.concat(this.users.get(i).toString());
+                    toReturn = toReturn.concat("\n");
+                }
+            }
+        }
+        return toReturn;
     }
-    @PostMapping("/tutorials")
-    public Tutorial createTutorial(@RequestBody Tutorial tutorial) {
-        return tutorialRepository
-                .save(new Tutorial(tutorial.getTitle(),
-                        tutorial.getDescription(), false));
-    }
-    @PutMapping("/tutorials/{id}")
-    public Tutorial updateTutorial(@PathVariable("id") long id,
-                                   @RequestBody Tutorial tutorial) {
-        Optional<Tutorial> tutorialData =
-                tutorialRepository.findById(id);
-        if (tutorialData.isPresent()) {
-            Tutorial _tutorial = tutorialData.get();
-            _tutorial.setTitle(tutorial.getTitle());
-            _tutorial.setDescription(tutorial.getDescription());
-            _tutorial.setPublished(tutorial.isPublished());
-            return tutorialRepository.save(_tutorial);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND); }
-    }
-    @DeleteMapping("/tutorials/{id}")
-    public HttpStatus deleteTutorial(@PathVariable("id") long id) {
-        tutorialRepository.deleteById(id);
-        return HttpStatus.NO_CONTENT; }
-    @DeleteMapping("/tutorials")
-    public HttpStatus deleteAllTutorials() {
-        tutorialRepository.deleteAll();
-        return HttpStatus.NO_CONTENT; }
-    @GetMapping("/tutorials/published")
-    public List<Tutorial> findByPublished() {
-        return tutorialRepository.findByPublished(true);
-    } }
+
+
+
+
+
+}
